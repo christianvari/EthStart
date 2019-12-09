5a565f479c467ff4fe7b83ee8ce2579ecf246526
Christian Vari <vari.christian@gmail.com>
2019-12-09 14:39:30 +0100

    can deploy on rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,28 +1,9 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
+const mnemonic = fs
+    .readFileSync(".secret")
+    .toString()
+    .trim();
 
 module.exports = {
     /**
@@ -46,7 +27,7 @@ module.exports = {
             host: "127.0.0.1", // Localhost (default: none)
             port: 8545, // Standard Ethereum port (default: none)
             network_id: "*" // Any network (default: none)
-        }
+        },
 
         // Another network with more advanced options...
         // advanced: {
@@ -60,14 +41,18 @@ module.exports = {
 
         // Useful for deploying to a public network.
         // NB: It's important to wrap the provider as a function.
-        // ropsten: {
-        // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-        // network_id: 3,       // Ropsten's id
-        // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-        // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-        // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-        // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-        // },
+        rinkeby: {
+            provider: () =>
+                new HDWalletProvider(
+                    mnemonic,
+                    "https://rinkeby.infura.io/v3/26a79c1bd63a44ab9d052322cd36c1ac"
+                ),
+            network_id: 4, // Ropsten's id
+            gas: 5500000, // Ropsten has a lower block limit than mainnet
+            confirmations: 2, // # of confs to wait between deployments. (default: 0)
+            timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+            skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
+        }
 
         // Useful for private networks
         // private: {
