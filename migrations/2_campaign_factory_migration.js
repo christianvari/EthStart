87f7b89070aac0b2c3bd81c71085f1b67166578e
Christian Vari <vari.christian@gmail.com>
2019-12-15 17:04:44 +0100

    added possibility to record address

--- migrations/2_campaign_factory_migration.js
+++ migrations/2_campaign_factory_migration.js
@@ -1,5 +1,12 @@
 const factory = artifacts.require("CampaignFactory");
+const fs = require("fs");
+const path = require("path");
 
-module.exports = function(deployer) {
-    deployer.deploy(factory);
+module.exports = async function(deployer) {
+    await deployer.deploy(factory);
+
+    fs.writeFileSync(
+        path.resolve(__dirname, "..", "build", "campaing_address"),
+        factory.address
+    );
 };
