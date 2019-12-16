2d0f00a3fa1b2fa587748da49e97339508694767
Christian Vari <vari.christian@gmail.com>
2019-12-16 20:30:30 +0100

    added getSummary function

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -81,4 +81,14 @@ contract Campaign {
         req.complete = true;
 
     }
+
+    function getSummary() public view returns(uint, uint, uint, uint, address){
+        return(
+            minimumContribution,
+            address(this).balance,
+            requests.length,
+            approversCount,
+            manager
+        );
+    }
 }
