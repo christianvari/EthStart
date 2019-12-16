c66978acbc389122d51172a437de9b48d5d20e7a
Christian Vari <vari.christian@gmail.com>
2019-12-16 16:36:28 +0100

    added input for minimum contribution

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
-import { Form, Button } from "semantic-ui-react";
+import { Form, Button, Input } from "semantic-ui-react";
 
 class CampaignNew extends Component {
+    state = {
+        minimumContribution: ""
+    };
+
     render() {
         return (
             <Layout>
@@ -11,7 +15,16 @@ class CampaignNew extends Component {
                 <Form>
                     <Form.Field>
                         <label>Minimum contribution</label>
-                        <input />
+                        <Input
+                            value={this.state.minimumContribution}
+                            onChange={event =>
+                                this.setState({
+                                    minimumContribution: event.target.value
+                                })
+                            }
+                            label="wei"
+                            labelPosition="right"
+                        />
                     </Form.Field>
 
                     <Button primary>Create!</Button>
