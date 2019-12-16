2d851cd6c159c8f446da1ace047e08cced16c609
Christian Vari <vari.christian@gmail.com>
2019-12-16 18:50:02 +0100

    added routing in header

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
+import Link from "next/link";
 
 export default () => {
     return (
         <Menu style={{ marginTop: "10px" }}>
-            <Menu.Item>KickStarter</Menu.Item>
+            <Link href="/">
+                <a className="item">KickStarter</a>
+            </Link>
+
             <Menu.Menu position="right">
-                <Menu.Item>Campaigns</Menu.Item>
-                <Menu.Item>+</Menu.Item>
+                <Link href="/">
+                    <a className="item">Campaigns</a>
+                </Link>
+                <Link href="/campaigns/new">
+                    <a className="item">+</a>
+                </Link>
             </Menu.Menu>
         </Menu>
     );
