18d7800e37db135709c4137b71b1ae273fbeec28
Christian Vari <vari.christian@gmail.com>
2019-12-16 20:17:27 +0100

    added support for useRouter in class component

--- pages/campaigns/[address].js
+++ pages/campaigns/[address].js
@@ -1,1 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
+import { useRouter } from "next/router";
+
+class CampaignShow extends Component {
+    render() {
+        return <h3>Campaign show {this.props.address}</h3>;
+    }
+}
+
+export default () => {
+    const router = useRouter();
+    const address = router.query.address;
+    return (
+        <div>
+            <CampaignShow address={address} />
+        </div>
+    );
+};
