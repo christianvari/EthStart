3bffe78addcedc6e0d99316fb8f28e4118a4268f
Christian Vari <vari.christian@gmail.com>
2019-12-16 12:50:59 +0100

    use semantic-ui cards to visualize campaigns

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 import factory from "../ethereum/factory";
+import { Card } from "semantic-ui-react";
 
 class CampaignIndex extends Component {
-    async componentDidMount() {
+    static async getInitialProps() {
+        // This is a NEXT.js method used to fetching information during server rendering
         const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-        console.log(campaigns);
+        return { campaigns };
+    }
+
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true
+            };
+        });
+
+        return <Card.Group items={items} />;
     }
 
     render() {
-        return <div>Campaigns index</div>;
+        return <div>{this.renderCampaigns()}</div>;
     }
 }
 
