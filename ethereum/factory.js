49a9fb1cacbd41316608ce4368e7f85989d310e6
Christian Vari <vari.christian@gmail.com>
2019-12-16 20:38:45 +0100

    Changed campaign factory instance address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import campaignFactory from "./build/contracts/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
     campaignFactory.abi,
-    "0x147f4A49dc5cf1Ee813c5fa2EE0867f0fbB9A0dd"
+    "0x14418847D74d342C5c345BeDfe59CCAb58E81c1B"
 );
 
 export default instance;
