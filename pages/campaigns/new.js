8802ce5961d1df144f733dbd6fdb205fe62645c2
Christian Vari <vari.christian@gmail.com>
2019-12-16 17:47:33 +0100

    added  form error handling

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,22 +1,31 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
-import { Form, Button, Input } from "semantic-ui-react";
+import { Form, Button, Input, Message } from "semantic-ui-react";
 import factory from "../../ethereum/factory";
 import web3 from "../../ethereum/web3";
 
 class CampaignNew extends Component {
     state = {
-        minimumContribution: ""
+        minimumContribution: "",
+        errorMessage: "",
+        loading: false
     };
 
     onSubmit = async event => {
         event.preventDefault();
-        const accounts = await web3.eth.getAccounts();
-        await factory.methods
-            .createCampaign(this.state.minimumContribution)
-            .send({
-                from: accounts[0]
-            });
+
+        this.setState({ loading: true, errorMessage: "" });
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods
+                .createCampaign(this.state.minimumContribution)
+                .send({
+                    from: accounts[0]
+                });
+        } catch (err) {
+            this.setState({ errorMessage: err.message });
+        }
+        this.setState({ loading: false });
     };
 
     render() {
@@ -24,7 +33,10 @@ class CampaignNew extends Component {
             <Layout>
                 <h3>Create a Campaign</h3>
 
-                <Form onSubmit={this.onSubmit}>
+                <Form
+                    onSubmit={this.onSubmit}
+                    error={!!this.state.errorMessage}
+                >
                     <Form.Field>
                         <label>Minimum contribution</label>
                         <Input
@@ -38,8 +50,14 @@ class CampaignNew extends Component {
                             labelPosition="right"
                         />
                     </Form.Field>
-
-                    <Button primary>Create!</Button>
+                    <Message
+                        error
+                        header="Oops"
+                        content={this.state.errorMessage}
+                    />
+                    <Button loading={this.state.loading} primary>
+                        Create!
+                    </Button>
                 </Form>
             </Layout>
         );
