69b3b4a0fa5b8a1280aa9cb664bcd4e99bec270d
Christian Vari <vari.christian@gmail.com>
2019-12-22 15:23:12 +0100

    added traslation layer

--- pages/campaigns/[address].js
+++ pages/campaigns/[address].js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
+import Campaign from "../../ethereum/campaign";
 
 class CampaignShow extends Component {
     static async getInitialProps(props) {
-        let address = props.query.address;
-        return { address };
+        const address = props.query.address;
+        const campaign = Campaign(address);
+
+        const summary = await campaign.methods.getSummary().call();
+
+        return {
+            minimumContribution: summary[0],
+            balance: summary[1],
+            requestsCount: summary[2],
+            approversCount: summary[3],
+            manager: summary[4]
+        };
     }
 
     render() {
         return (
             <Layout>
-                <h3>Campaign show {this.props.address}</h3>
+                <h3>Campaign show</h3>
             </Layout>
         );
     }
