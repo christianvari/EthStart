29a15fb9ac6008047da455073dd8ca28a428e31f
Christian Vari <vari.christian@gmail.com>
2019-12-22 15:59:35 +0100

    added form to contribute

--- pages/campaigns/[address].js
+++ pages/campaigns/[address].js
@@ -3,6 +3,7 @@ import Layout from "../../components/Layout";
 import Campaign from "../../ethereum/campaign";
 import { Card } from "semantic-ui-react";
 import web3 from "../../ethereum/web3";
+import ContributeForm from "../../components/ContributeForm";
 
 class CampaignShow extends Component {
     static async getInitialProps(props) {
@@ -73,6 +74,7 @@ class CampaignShow extends Component {
             <Layout>
                 <h3>Campaign details</h3>
                 {this.renderCards()}
+                <ContributeForm />
             </Layout>
         );
     }
