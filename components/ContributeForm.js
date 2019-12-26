516e42e509fceff3953ef3f5612dc377919b1671
Christian Vari <vari.christian@gmail.com>
2019-12-26 17:06:57 +0100

    user can contribute

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,13 +1,41 @@
 import React, { Component } from "react";
 import { Form, Input, Message, Button } from "semantic-ui-react";
+import Campaign from "../ethereum/campaign";
+import web3 from "../ethereum/web3";
+import Router from "next/router";
 
 class ContributeForm extends Component {
+    state = { value: "" };
+
+    onSubmit = async event => {
+        event.preventDefault();
+        const campaign = Campaign(this.props.address);
+
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods.contribute().send({
+                from: accounts[0],
+                value: web3.utils.toWei(this.state.value)
+            });
+            Router.replace(`/campaigns/${this.props.address}`);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     render() {
         return (
-            <Form>
+            <Form onSubmit={this.onSubmit}>
                 <Form.Field>
                     <label>Amount to contribute</label>
-                    <Input label="ether" labelPosition="right" />
+                    <Input
+                        onChange={event => {
+                            this.setState({ value: event.target.value });
+                        }}
+                        value={this.state.value}
+                        label="ether"
+                        labelPosition="right"
+                    />
                 </Form.Field>
                 <Button primary>Contribute!</Button>
             </Form>
--- pages/campaigns/[address].js
+++ pages/campaigns/[address].js
@@ -13,6 +13,7 @@ class CampaignShow extends Component {
         const summary = await campaign.methods.getSummary().call();
 
         return {
+            address: address,
             minimumContribution: summary[0],
             balance: summary[1],
             requestsCount: summary[2],
@@ -76,7 +77,7 @@ class CampaignShow extends Component {
                 <Grid>
                     <Grid.Column width={10}>{this.renderCards()}</Grid.Column>
                     <Grid.Column width={6}>
-                        <ContributeForm />
+                        <ContributeForm address={this.props.address} />
                     </Grid.Column>
                 </Grid>
             </Layout>
