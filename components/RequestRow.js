968c12afafe7c1e7067cd133054287aec8cfeb77
Christian Vari <vari.christian@gmail.com>
2019-12-27 16:06:04 +0100

    added styling to button

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -2,30 +2,43 @@ import React, { Component } from "react";
 import { Table, Button } from "semantic-ui-react";
 import web3 from "../ethereum/web3";
 import Campaign from "../ethereum/campaign";
+import Router from "next/router";
 
 class RequestRow extends Component {
+    state = { loadingApprove: false, loadingFinalize: false };
+
     onApprove = async () => {
+        this.setState({ loadingApprove: true });
         const campaign = Campaign(this.props.address);
         const accounts = await web3.eth.getAccounts();
         await campaign.methods
             .approveRequest(this.props.id)
             .send({ from: accounts[0] });
+        this.setState({ loadingApprove: false });
+        Router.push(`/campaigns/${this.props.address}/requests`);
     };
 
     onFinalize = async () => {
+        this.setState({ loadingFinalize: true });
         const campaign = Campaign(this.props.address);
         const accounts = await web3.eth.getAccounts();
         await campaign.methods
             .finalizeRequest(this.props.id)
             .send({ from: accounts[0] });
+        this.setState({ loadingFinalize: false });
+        Router.push(`/campaigns/${this.props.address}/requests`);
     };
 
     render() {
         const { Row, Cell } = Table;
         const { id, request, approversCount } = this.props;
+        const readyToFinalize = request.approvalCount > approversCount / 2;
 
         return (
-            <Row>
+            <Row
+                disabled={request.complete}
+                positive={readyToFinalize && !request.complete}
+            >
                 <Cell>{id}</Cell>
                 <Cell>{request.description}</Cell>
                 <Cell>{web3.utils.fromWei(request.value, "ether")}</Cell>
@@ -34,14 +47,28 @@ class RequestRow extends Component {
                     {request.approvalCount}/{approversCount}
                 </Cell>
                 <Cell>
-                    <Button color={"green"} basic onClick={this.onApprove}>
-                        Approve
-                    </Button>
+                    {request.complete ? null : (
+                        <Button
+                            loading={this.state.loadingApprove}
+                            color={"green"}
+                            basic
+                            onClick={this.onApprove}
+                        >
+                            Approve
+                        </Button>
+                    )}
                 </Cell>
                 <Cell>
-                    <Button color={"teal"} basic onClick={this.onFinalize}>
-                        Finalize
-                    </Button>
+                    {request.complete ? null : (
+                        <Button
+                            loading={this.state.loadingFinalize}
+                            color={"teal"}
+                            basic
+                            onClick={this.onFinalize}
+                        >
+                            Finalize
+                        </Button>
+                    )}
                 </Cell>
             </Row>
         );
