eb0ead1685c0b3854a5d14979c263ab7c987278f
Christian Vari <vari.christian@gmail.com>
2019-12-27 13:35:28 +0100

    added table

--- pages/campaigns/[address]/requests.js
+++ pages/campaigns/[address]/requests.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
 import Layout from "../../../components/Layout";
 import Link from "next/link";
-import { Button } from "semantic-ui-react";
+import { Button, Table } from "semantic-ui-react";
+import Campaing from "../../../ethereum/campaign";
 
 class RequestIndex extends Component {
     static async getInitialProps(props) {
         const address = props.query.address;
-        return { address };
+
+        const campaign = Campaing(address);
+        const requestsCount = await campaign.methods.getRequestsCount().call();
+        const requests = await Promise.all(
+            Array(parseInt(requestsCount))
+                .fill()
+                .map((element, index) => {
+                    return campaign.methods.requests(index).call();
+                })
+        );
+
+        return { address, requests };
     }
     render() {
+        const { Header, Row, HeaderCell, Body } = Table;
+
         return (
             <Layout>
                 <h3>Requests</h3>
@@ -17,6 +31,20 @@ class RequestIndex extends Component {
                         <Button primary>Add Request</Button>
                     </a>
                 </Link>
+
+                <Table>
+                    <Header>
+                        <Row>
+                            <HeaderCell>ID</HeaderCell>
+                            <HeaderCell>Description</HeaderCell>
+                            <HeaderCell>Amount</HeaderCell>
+                            <HeaderCell>Recipient</HeaderCell>
+                            <HeaderCell>Approval Count</HeaderCell>
+                            <HeaderCell>Approve</HeaderCell>
+                            <HeaderCell>Finalize</HeaderCell>
+                        </Row>
+                    </Header>
+                </Table>
             </Layout>
         );
     }
